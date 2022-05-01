897ed72679367a294de68435f8195c63ac416acd
태깅 <min4644@gmail.com>
2022-05-01 19:48:29 +0900

    feat: Create Project

---
+++ Hi-G/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hi-G
+//
+//  Created by 김민택 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
