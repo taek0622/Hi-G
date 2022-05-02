a4cb2d590e52ab9758281fa7565800936b187ab0
태깅 <min4644@gmail.com>
2022-05-02 17:21:18 +0900

    feat: Add Some Lists

---
+++ Hi-G/BarsListView.swift
@@ -0,0 +1,14 @@
+//
+//  BarsListView.swift
+//  Hi-G
+//
+//  Created by 김민택 on 2022/05/02.
+//
+
+import SwiftUI
+
+struct BarsListView: View {
+    var body: some View {
+        Text("BarsList")
+    }
+}
---
+++ Hi-G/ControlsListView.swift
@@ -0,0 +1,14 @@
+//
+//  ControlsListView.swift
+//  Hi-G
+//
+//  Created by 김민택 on 2022/05/02.
+//
+
+import SwiftUI
+
+struct ControlsListView: View {
+    var body: some View {
+        Text("ControlsList")
+    }
+}
